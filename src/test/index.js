e71ff262ee24e2e2fb391fd9fb561c5ebb67f421
Kadek Sastrawan <sastrawanikadek@gmail.com>
2019-08-20 15:24:50 +0800

    Update index.js

--- src/test/index.js
+++ src/test/index.js
@@ -1,45 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import Paper from "../components/Paper";
-import TextInput from "../components/TextInput";
-import Icon from "../components/Icon";
-import { createUseStyles } from "react-jss";
-import { globalStyles } from "../utils/styles";
-
-const useStyles = createUseStyles({
-  tableHead: {
-    display: "flex",
-    padding: 16
-  }
-});
-
-const useGlobalStyles = createUseStyles(globalStyles);
-
-const Test = props => {
-  const classes = useStyles();
-  const styles = useGlobalStyles();
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        backgroundColor: styles.bgBabyBlue
-      }}
-    >
-      <Icon color="pink">shield-account</Icon>
-      {/* <Paper style={{ width: 876, borderRadius: 4 }}>
-        <div className={classes.tableHead}>
-          <h1 style={{ flex: 1 }}>Ini Table Head</h1>
-          <TextInput
-            placeholder="Search"
-            label="Search"
-            extra={{ start: <SearchIcon />, end }}
-          />
-        </div>
-      </Paper> */}
-=======
-import AppBar from "../components/AppBar";
 import { globalStyles } from "../utils/styles";
 import { createUseStyles } from "react-jss";
 
@@ -47,12 +6,8 @@ const useGlobalStyles = createUseStyles(globalStyles);
 
 const Test = props => {
   const styles = useGlobalStyles();
-  return (
-    <div>
-      <AppBar title="Title" profile="Profile" showMenu />
->>>>>>> 90c30db1588ce1fd009e5e9c242b9f8d6404021b
-    </div>
-  );
+
+  return <div className={styles.posRelative} />;
 };
 
 export default Test;
