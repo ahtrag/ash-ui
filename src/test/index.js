bb272daf2455e5588b5c8e48e65bca3238d3872c
ahtrag <garthaprasidhiyanta@gmail.com>
2019-08-20 12:30:16 +0800

    ASH-007 Create Divider Component and add into drawer, fix global style button view

--- src/test/index.js
+++ src/test/index.js
@@ -1,15 +1,37 @@
 import React from "react";
-import AppBar from "../components/AppBar";
+import AppDrawer from "../components/AppDrawer";
 import { globalStyles } from "../utils/styles";
 import { createUseStyles } from "react-jss";
+import MenuIcon from "mdi-react/MenuIcon";
+import Button from "../components/Button";
 
 const useGlobalStyles = createUseStyles(globalStyles);
+
+const menuList = [
+  {
+    icon: <MenuIcon />,
+    label: "Yui",
+    to: "/",
+    divider: true
+  }
+];
 
 const Test = props => {
   const styles = useGlobalStyles();
   return (
-    <div>
-      <AppBar title="Title" profile="Profile" showMenu />
+    <div className={styles.posRelative}>
+      <AppDrawer title="Title" profile="Profile" showMenu menuList={menuList}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        <Button>Kadek</Button>
+      </AppDrawer>
     </div>
   );
 };
