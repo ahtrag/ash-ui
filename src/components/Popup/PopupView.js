0d09fffc6b74ab3c955d2dd30dff7d67856a1b2c
Kadek Sastrawan <sastrawanikadek@gmail.com>
2019-09-04 20:53:18 +0800

    ASH - 019 - Create Popup Component

---
+++ src/components/Popup/PopupView.js
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Paper from "../Paper";
+import PropTypes from "prop-types";
+import { createUseStyles } from "react-jss";
+import { CSSTransition } from "react-transition-group";
+import { renderStyle, renderClassName } from "../../utils/constants";
+
+const useStyles = createUseStyles({
+  root: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%"
+  },
+  popupEnter: {
+    "& $paper": {
+      transform: "scale(1)",
+      opacity: 1
+    }
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%"
+  },
+  paper: {
+    position: "absolute",
+    overflowX: "hidden",
+    overflowY: "auto",
+    top: props => props.top,
+    left: props => props.left,
+    zIndex: 100,
+    maxWidth: "calc(100% - 32px)",
+    maxHeight: 200,
+    transformOrigin: props => props.transformOrigin,
+    transform: "scale(0)",
+    opacity: 0,
+    transition: "transform 0.2s ease-in-out, opacity 0.2s ease-in-out"
+  }
+});
+
+const PopupView = props => {
+  const {
+    children,
+    target,
+    position,
+    transformOrigin,
+    onClose,
+    className,
+    style
+  } = props;
+  const [element, setElement] = useState(null);
+  const popupPosition = renderStyle(
+    { vertical: "top", horizontal: "left" },
+    position
+  );
+  const transformPosition = renderStyle(
+    { vertical: "center", horizontal: "center" },
+    transformOrigin
+  );
+  const styleProps = {
+    top: 0,
+    left: 0,
+    transformOrigin: `${transformPosition.horizontal} ${transformPosition.vertical}`
+  };
+
+  if (element) {
+    styleProps.top = element.offsetTop;
+    styleProps.left = element.offsetLeft;
+
+    if (popupPosition.vertical === "center") {
+      styleProps.top = element.offsetTop + element.offsetHeight / 2;
+    } else if (popupPosition.vertical === "bottom") {
+      styleProps.top = element.offsetTop + element.offsetHeight;
+    }
+
+    if (popupPosition.horizontal === "center") {
+      styleProps.left = element.offsetLeft + element.offsetWidth / 2;
+    } else if (popupPosition.horizontal === "right") {
+      styleProps.left = element.offsetLeft + element.offsetWidth;
+    }
+  }
+
+  const classes = useStyles(styleProps);
+
+  useEffect(() => {
+    if (target) {
+      setElement(target);
+      document.getElementsByTagName("html")[0].style.overflow = "hidden";
+    }
+
+    return () => (document.getElementsByTagName("html")[0].style.overflow = "");
+  }, [target]);
+
+  return ReactDOM.createPortal(
+    <CSSTransition
+      in={Boolean(target)}
+      timeout={200}
+      classNames={{
+        enterActive: classes.popupEnter,
+        enterDone: classes.popupEnter
+      }}
+      unmountOnExit
+    >
+      <div className={classes.root}>
+        <div className={classes.overlay} onClick={onClose} />
+
+        <Paper
+          className={renderClassName(classes.paper, className)}
+          style={style}
+        >
+          {children}
+        </Paper>
+      </div>
+    </CSSTransition>,
+    document.getElementsByTagName("body")[0]
+  );
+};
+
+PopupView.propTypes = {
+  children: PropTypes.any.isRequired,
+  target: PropTypes.object,
+  position: PropTypes.shape({
+    vertical: PropTypes.oneOf(["top", "center", "bottom"]),
+    horizontal: PropTypes.oneOf(["left", "center", "right"])
+  }),
+  transformOrigin: PropTypes.shape({
+    vertical: PropTypes.oneOf(["top", "center", "bottom"]),
+    horizontal: PropTypes.oneOf(["left", "center", "right"])
+  }),
+  onClose: PropTypes.func,
+  className: PropTypes.string,
+  style: PropTypes.object
+};
+
+export default PopupView;
