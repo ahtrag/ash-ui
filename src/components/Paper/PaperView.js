2ee20c7001678bd0ae897acf82e709cc7d1e236f
Kadek Sastrawan <sastrawanikadek@gmail.com>
2019-09-18 21:03:14 +0800

    ASH - 011 - Update Component Style

--- src/components/Paper/PaperView.js
+++ src/components/Paper/PaperView.js
@@ -7,7 +7,8 @@ import { renderClassName } from "../../utils/constants";
 const useStyles = createUseStyles({
   root: {
     backgroundColor: "white",
-    borderRadius: 4
+    borderRadius: 4,
+    overflow: "hidden"
   },
   shadowLower: {
     boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)"
